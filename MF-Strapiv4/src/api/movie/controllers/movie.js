5194b046f50403135ae252d7faf7d02fde31f4a3
isaac harris <isaachrr4@gmail.com>
2023-02-02 15:57:35 -0500

    strapi playground

---
+++ MF-Strapiv4/src/api/movie/controllers/movie.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * movie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::movie.movie');
+ 
+
+   
+
